583e9b1bf79a3a5a8e0e88f4f83781c711d5c175
manivijayanand <manivijayanand@yahoo.com>
2021-03-21 08:18:38 -0500

    Add files via upload

---
+++ src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/getInfo")
+	public String getInfo() {
+		return Hello World";
+	}
+}
